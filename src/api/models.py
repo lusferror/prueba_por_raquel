f3a846f7798f9c311053075345c509c2ef1c58dc
LUIS ALFREDO ROBLES <110691633+lusferror@users.noreply.github.com>
2023-05-06 02:16:30 +0000

    Pimeros Cambios

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    rut= db.Column(db.String(100), nullable=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
